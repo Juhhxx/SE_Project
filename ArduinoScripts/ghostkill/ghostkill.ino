04c33f61c85fdf05cd39eae5c516e51dbf89b54b
Julia Costa <juliacosta232005@gmail.com>
2025-09-11 17:30:46 +0100

    Add Code to Send/Recieve byte Data via UDP Packages to Arduino

--- ArduinoScripts/ghostkill/ghostkill.ino
+++ ArduinoScripts/ghostkill/ghostkill.ino
@@ -1,3 +1,5 @@
+#include "WiFi.h"
+#include "AsyncUDP.h"
 #include <MD_MAX72xx.h>
 
 #define PRINT(s, x) { Serial.print(F(s)); Serial.print(x); }
@@ -7,9 +9,9 @@
 #define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
 #define MAX_DEVICES	1
 
-#define CLK_PIN   8  // or SCK
-#define DATA_PIN  10  // or MOSI
-#define CS_PIN    9  // or SS
+#define CLK_PIN   18 // or SCK
+#define DATA_PIN  23  // or MOSI
+#define CS_PIN    13  // or SS
 
 // SPI hardware interface
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
@@ -17,16 +19,8 @@ MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 // We always wait a bit between updates of the display
 #define  DELAYTIME  100  // in milliseconds
 
-void setup()
-{
-  Serial.begin(57600);
-
-  PRINTS("\n[MD_MAX72XX Test & Demo]");
-
-  if (!mx.begin())
-    PRINTS("\nMD_MAX72XX initialization failed");
-  
-}
+const char *ssid = "Redmi 10A";
+const char *password = "12345678";
 
 void draw(uint8_t bitmap[8])
 {
@@ -39,9 +33,62 @@ void draw(uint8_t bitmap[8])
   delay(DELAYTIME);
 }
 
-void loop()
-{
-   uint8_t arrow[COL_SIZE] =
+AsyncUDP udp;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  if (!mx.begin())
+    PRINTS("\nMD_MAX72XX initialization failed");
+
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed");
+    while (1) {
+      delay(1000);
+    }
+  }
+  if (udp.listen(1234)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      Serial.print("UDP Packet Type: ");
+      Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
+      Serial.print(", From: ");
+      Serial.print(packet.remoteIP());
+      Serial.print(":");
+      Serial.print(packet.remotePort());
+      Serial.print(", To: ");
+      Serial.print(packet.localIP());
+      Serial.print(":");
+      Serial.print(packet.localPort());
+      Serial.print(", Length: ");
+      Serial.print(packet.length());
+      Serial.print(", Data: ");
+      Serial.write(packet.data(), packet.length());
+      Serial.println();
+
+      if (packet.length() == 8) {
+          uint8_t bitmap[8];
+          memcpy(bitmap, packet.data(), 8);  // copy bytes from UDP packet
+          draw(bitmap);                       // call your method
+      }
+      //reply to the client
+      packet.printf("Got %u bytes of data", packet.length());
+    });
+  }
+}
+
+void loop() {
+
+  delay(1000);
+  //Send broadcast
+  udp.broadcast("Anyone here?");
+
+  uint8_t arrow[COL_SIZE] =
   {
     0b11111111,
     0b00011100,
@@ -52,7 +99,5 @@ void loop()
     0b00111110,
     0b00111100
   };
-
-  draw(arrow);
 
 }
